0d01321bfd0b86685b07ed3b92d8d8eb8c5d147f
Joyce Abou Rjeily <joyce@bold.com.sa>
2023-03-02 11:11:31 +0300

    setup project

---
+++ Frontend/src/components/Header/Header.data.js
@@ -0,0 +1,15 @@
+export const NAV_LINKS = [
+    { link: "/about-us", isDesktop: true, isMobile: true, label: "About NEC" },
+    { link: "/initiatives", isDesktop: true, isMobile: true, label: "Initiatives" },
+    { link: "/sponsorship", isDesktop: true, isMobile: true, label: "Sponsorship" },
+    { link: "/media-center", isDesktop: true, isMobile: true, label: "Media center" },
+    { link: "/join-us", isDesktop: true, isMobile: true, label: "Join NEC" },
+    { link: "/contact-us", isDesktop: false, isMobile: true, label: "Contact NEC" },
+];
+
+export const NAV_CONTACT = [
+    { link: "/contact-us", isDesktop: true, isMobile: true, label: "Contact NEC" },
+]
+
+
+export const MOBILE_MENU_EXIT_DURATION = 700;
---
+++ Frontend/src/components/Header/Header.js
@@ -0,0 +1,74 @@
+//import { useState, useRef } from "react";
+import Link from "next/link";
+//import { CSSTransition } from "react-transition-group";
+import cx from "classnames";
+import { NAV_LINKS } from "./Header.data";
+import styles from "./Header.module.scss";
+import Image from "next/image";
+
+import logoVert from '../../assets/logo-vert.svg';
+import logo from '../../assets/logo.svg';
+import contactIcon from '../../assets/ic-contact.svg';
+import Menu from "../Menu/Menu";
+
+const Header = ({ }) => {
+    return (
+        <>
+            <header
+                className={cx(styles.header)}
+            >
+                <div className={styles.headerContainer}>
+                    <Link href="./" className={styles.logo+" d-none d-md-block"}>
+                        <Image src={logo} alt="logo" />
+                    </Link>
+                    <Link href="./" className={styles.logo+" d-block d-md-none"}>
+                        <Image src={logoVert} alt="logo" />
+                    </Link>
+
+                    <div className={styles.menuContainer}>
+                        <ul className={styles.menu}>
+                            {NAV_LINKS.map(({ link, label,isDesktop }, index) => {
+                                return isDesktop && <li key={index}>
+                                <Link href={link} scroll={false}>
+                                    <span
+                                        className={cx({
+                                            //  [styles.activeNavLink]: activePath === link,
+                                            [styles.activeNavLink]: true
+                                        })}
+                                    >
+                                        {label}
+                                    </span>
+                                </Link>
+                            </li>
+                            })}
+                        </ul>
+                    </div>
+
+                    <div className={styles.headerButton}>
+                        <Link href={'/contact-us'} scroll={false}>
+                            <span
+                                className={'font-weight-bold paragraph1-size'}
+                            >
+                                Contact NEC
+                            </span>
+
+                            <span>
+                                <Image src={contactIcon} alt="contact"/>
+                            </span>
+                        </Link>
+                    </div>
+                    <div className={styles.menuContainer+" d-block d-md-none"}>
+
+                        <div className={styles.menu}>
+                            <Menu menuItems={NAV_LINKS} className={styles.Menu}
+                            ></Menu>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+        </>
+    );
+};
+
+export default Header;

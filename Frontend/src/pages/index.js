d7d18429ba2333d7f380953646c23ecb2a0b936e
Joyce Abou Rjeily <joyce@bold.com.sa>
2023-03-12 16:28:22 +0300

    fix analytics

--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -11,46 +11,15 @@ import LangSwitch from "./../components/LangSwitch/LangSwitch";
 export default function Home({ locale }) {
   return (
     <>
-      <Head>
-        <title>Abdelrahman Oasis</title>
-        <meta name="description" content="Abdelrahman Oasis" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
 
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=G-G4WL8WZDPW`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', 'G-G4WL8WZDPW');
-                            `,
-          }}
-        />
 
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','G-G4WL8WZDPW');`,
-          }}
-        />
-      </Head>
-      <>
-        <LangSwitch />
-        <Who />
-        <Volunteer locale={locale} />
-        <Publication />
-        <Donate />
-        <Calender />
-      </>
+      <LangSwitch />
+      <Who />
+      <Volunteer locale={locale} />
+      <Publication />
+      <Donate />
+      <Calender />
+
     </>
   );
 }

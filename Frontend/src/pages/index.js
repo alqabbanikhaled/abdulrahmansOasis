cb484918b38b7144d67aad0f3556d1aaf6cff39b
Mohamed Ramadan <m.ramadan@bold.com.sa>
2023-03-16 12:52:06 +0200

    home fetch

--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useEffect } from "react";
 
 import Who from "./../components/home/Who/Who";
 import Donate from "./../components/home/Donate/Donate";
@@ -8,8 +9,19 @@ import Calender from "./../components/home/Calender/Calender";
 
 import LangSwitch from "./../components/LangSwitch/LangSwitch";
 import Header from "./../components/Header/Header";
+import { getSinglePage } from "@/providers/api.service";
 
-export default function Home({ locale }) {
+export default function Home({ locale, home }) {
+  // useEffect(async () => {
+  //   const data = await getSinglePage(
+  //     "home-page",
+  //     "volunteer,publicaiton,donate,calender,who"
+  //   );
+  //   console.log(data);
+  // }, []);
+
+  console.log(data);
+
   return (
     <>
       {/* <LangSwitch /> */}
@@ -24,9 +36,15 @@ export default function Home({ locale }) {
 }
 
 export async function getServerSideProps({ locale }) {
+  const data = await getSinglePage(
+    "home-page",
+    "volunteer,publicaiton,donate,calender,who"
+  );
+
   return {
     props: {
       locale: locale,
+      data,
     },
   };
 }
---
+++ Frontend/src/providers/api.service.js
@@ -0,0 +1,22 @@
+const baseUrl = "http://localhost:1337";
+
+export async function getSinglePage(name, fields) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  const url = `${baseUrl}/api/${name}?populate=${fields}`;
+  try {
+    const res = await fetch(url, requestOptions);
+    const fetchJson = await res.json();
+    console.log(fetchJson);
+    return fetchJson;
+  } catch (e) {
+    throw e;
+  }
+}

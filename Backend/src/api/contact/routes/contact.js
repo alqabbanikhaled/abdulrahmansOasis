4fa0d4538b37f1dcaa406e5ad3087efa5ba4da5c
Joyce Abou Rjeily <joyce@bold.com.sa>
2023-03-23 11:12:39 +0300

    add recaptcha

--- Backend/src/api/contact/routes/contact.js
+++ Backend/src/api/contact/routes/contact.js
@@ -6,4 +6,32 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::contact.contact');
+module.exports = createCoreRouter('api::contact.contact', {
+    config: {
+        find: {
+            policies: [{
+                name: 'global::recaptcha',
+            }],
+        },
+        findOne: {
+            policies: [{
+                name: 'global::recaptcha',
+            }],
+        },
+        create: {
+            policies: [{
+                name: 'global::recaptcha',
+            }],
+        },
+        update: {
+            policies: [{
+                name: 'global::recaptcha',
+            }],
+        },
+        delete: {
+            policies: [{
+                name: 'global::recaptcha',
+            }],
+        },
+    },
+});

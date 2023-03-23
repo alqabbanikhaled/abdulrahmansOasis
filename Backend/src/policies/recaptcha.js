9f2c04b2da0872b9e1e419bde091afe13fcca485
Joyce Abou Rjeily <joyce@bold.com.sa>
2023-03-23 12:09:26 +0300

    add logs

--- Backend/src/policies/recaptcha.js
+++ Backend/src/policies/recaptcha.js
@@ -1,16 +1,14 @@
 const axios = require("axios");
-require("dotenv");
 module.exports = async (ctx, next) => {
   // url params
   const { token } = ctx.request.body;
   const remoteip = ctx.req.connection.remoteAddress;
-  const secret_key = '6LdRJSElAAAAALFn8aXfrOBCZ9BkohlZfQq4kvaj';
 
-  //console.log("captcha key is:" + secret_key);
-  //console.log("token is:" + token);
-  //console.log("remote IP:" + remoteip);
+  console.log("captcha key is:" + secret_key);
+  console.log("token is:" + token);
+  console.log("remote IP:" + remoteip);
 
-  const verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}&remoteip=${remoteip}`;
+  const verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=6LdRJSElAAAAALFn8aXfrOBCZ9BkohlZfQq4kvaj&response=${token}&remoteip=${remoteip}`;
   let validation = await axios.post(verifyURL).then(function (response) {
     if (response.data.success !== undefined && !response.data.success) {
       console.log("you should not pass" + response.data.success);

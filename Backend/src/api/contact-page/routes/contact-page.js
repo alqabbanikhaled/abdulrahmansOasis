501148d0965d54e3dee96b97e36c2ab3a884a5b9
Joyce Abou Rjeily <joyce@bold.com.sa>
2023-03-23 10:03:42 +0300

    remove recaptcha

--- Backend/src/api/contact-page/routes/contact-page.js
+++ Backend/src/api/contact-page/routes/contact-page.js
@@ -6,18 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::contact-page.contact-page', {
-    config: {
-        create: {
-            policies: [
-                // point to a registered policy
-                "global::recaptcha",
-
-                // pass a policy implementation directly
-                (policyContext, config, { strapi }) => {
-                    return true;
-                },
-            ]
-        }
-    }
-});
+module.exports = createCoreRouter('api::contact-page.contact-page');
